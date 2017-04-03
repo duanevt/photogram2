8e871884814d7d95856a6eeefa52a58228082f37
Willian Ribeiro Angelo <agfoccus@gmail.com>
2017-04-02 22:11:23 -0300

    Terms App Name

--- src/pages/terms/terms.ts
+++ src/pages/terms/terms.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import {APP_NAME} from '../../config'
 
-/*
-  Generated class for the Terms page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-terms',
   templateUrl: 'terms.html'
 })
 export class TermsPage {
+  appName: string = APP_NAME;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
